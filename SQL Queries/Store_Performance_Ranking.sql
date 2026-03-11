212af0cbbcb409e18cda2849e9b03bbc8f27adb1
Pallab Dey <pallabdey21@gmail.com>
2026-03-11 15:51:38 +0600

    docs: update the alignment issues of aliases

--- SQL Queries/Store_Performance_Ranking.sql
+++ SQL Queries/Store_Performance_Ranking.sql
@@ -10,18 +10,18 @@ select
 	Store_Location,
 	Store_City,
 
-	count(*)		as transaction_count,
-	sum(units)		as total_units_sold,
-	round(sum(revenue),2) as total_revenue,
-	round(sum(cost_of_goods_sold),2) as total_cogs,
-	round(sum(gross_profit),2) as total_gross_profit,
-	round(avg(gross_margin_pct),2) as avg_gross_margin_pct,
+	count(*)													as transaction_count,
+	sum(units)												    as total_units_sold,
+	round(sum(revenue),2)										as total_revenue,
+	round(sum(cost_of_goods_sold),2)							as total_cogs,
+	round(sum(gross_profit),2)									as total_gross_profit,
+	round(avg(gross_margin_pct),2)								as avg_gross_margin_pct,
 
-	round( 100 * sum(revenue) / sum(sum(revenue)) over (), 2) as pct_of_total_revenue,
+	round( 100 * sum(revenue) / sum(sum(revenue)) over (), 2)	as pct_of_total_revenue,
 	
-	rank() over(order by sum(revenue) desc) as revenue_rank,
-	rank() over(order by sum(gross_profit) desc) as gross_profit_rank,
-	rank() over(order by avg(gross_margin_pct) desc) as margin_rank
+	rank() over(order by sum(revenue) desc)						as revenue_rank,
+	rank() over(order by sum(gross_profit) desc)				as gross_profit_rank,
+	rank() over(order by avg(gross_margin_pct) desc)			as margin_rank
 
 from vw_Sales_Enriched
 
